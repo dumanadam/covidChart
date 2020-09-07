7707dad62d769a55181789d66828057fa97d58c0
Talha <tasezgi.n@gmail.com>
2020-09-08 07:50:23 +1000

    tested working

--- README.md
+++ README.md
@@ -1,3 +1,99 @@
-# covidChart
+# 2020 Grad Covid Tracker
 
-Display the latest Vic and West Australian Covid data using chartjs
+> Annexa grad Tracker
+
+> Display latest Covid Recoveries
+
+[![Build Status](http://img.shields.io/travis/badges/badgerbadgerbadger.svg?style=flat-square)](https://travis-ci.org/badges/badgerbadgerbadger) [![License](http://img.shields.io/:license-mit-blue.svg?style=flat-square)](http://badges.mit-license.org)
+**_Overlay states_**
+[![Talha](https://i.imgur.com/UvsMEQi.png)](https://i.imgur.com/UvsMEQi.png)
+
+[![OPTIONAL OVERLAY](https://i.imgur.com/WbKSoZv.png)]()
+
+## Table of Contents
+
+- [Information](#information)
+- [Settings](#settings)
+- [Installation](#installation)
+- [Features](#features)
+- [License](#license)
+
+---
+
+## Information
+
+Libraries used :
+
+- Papa Parse for CSV parsing > https://www.papaparse.com/
+- Chartjs for charting > https://www.chartjs.org/
+- Bootstrap
+- Jquery
+
+Written in as much vanilla JS as possible.
+Pulls the latest Covid data in CSV format
+Formats into an object which is used in chartjs
+
+---
+
+## Settings (Optional)
+
+To change any settings at the top of annexa.js are options to change colours, text and the source URL.
+
+```javascript
+//********************************* SETTINGS START********************************** */
+
+//Update URL as required
+let latestCSVUrl =
+  "https://raw.githubusercontent.com/M3IT/COVID-19_Data/master/Data/COVID_AU_state_daily_change.csv";
+
+// Text for Chart Title
+const headerTitle = "2020 Grad Covid Tracker";
+const labelText = "Monthly Recovered";
+const footer = "Talha Sezgin - MIT license";
+const footer2 = " Datasource >> " + latestCSVUrl;
+
+//set colours for chart [low, med, high]
+let chartjsBarColours = [
+  "rgba(0, 255, 21, 0.39)",
+  "rgba(214, 198, 41, 0.39)",
+  "rgba(223, 32, 56, 0.39)",
+];
+
+//Set the ranges for  low med high colours
+let medColour = 499;
+let highColour = 1500;
+
+// Month used for current CSV format to extract date. Adjustment only needed if CSV format changes
+const minMonthPos = 5;
+const maxMonthPos = 7;
+
+//States to be charted
+const states = ["VIC", "WA"];
+
+//********************************* SETTINGS END********************************** */
+```
+
+---
+
+## Installation
+
+- All the `code` required to get going is in the repository
+- copy + paste index.html, annexa,js and the assets folder
+  or
+- clone
+- Open in your browser of choice (tested in FF)
+
+---
+
+## Features
+
+- Button to overlay state over another
+- Listbox dynamically shows recovered and raw data
+
+---
+
+## License
+
+[![License](http://img.shields.io/:license-mit-blue.svg?style=flat-square)](http://badges.mit-license.org)
+
+- **[MIT license](http://opensource.org/licenses/mit-license.php)**
--- assets/js/annexa.js
+++ assets/js/annexa.js
@@ -1,11 +1,14 @@
-// Text for Chart Title
-let recoveryText = "Victorian Recoveries as of: ";
+//********************************* SETTINGS START********************************** */
 
-// variables for storage and filtering
-let rawCSVData,
-  rejectedStates,
-  chartjsdata = [];
-let csvSplitByMonth = [];
+//Update URL as required
+let latestCSVUrl =
+  "https://raw.githubusercontent.com/M3IT/COVID-19_Data/master/Data/COVID_AU_state_daily_change.csv";
+
+// Text for Chart Title
+const headerTitle = "2020 Grad Covid Tracker";
+const labelText = "Monthly Recovered";
+const footer = "Talha Sezgin - MIT license";
+const footer2 = " Datasource >> " + latestCSVUrl;
 
 //set colours for chart [low, med, high]
 let chartjsBarColours = [
@@ -13,15 +16,26 @@ let chartjsBarColours = [
   "rgba(214, 198, 41, 0.39)",
   "rgba(223, 32, 56, 0.39)",
 ];
+
+//Set the ranges for  low med high colours
 let medColour = 499;
 let highColour = 1500;
 
-// Month used for current CSV format to extract date. Adjust if CSV changes
+// Month used for current CSV format to extract date. Adjustment only needed if CSV format changes
 const minMonthPos = 5;
 const maxMonthPos = 7;
 
 //States to be charted
 const states = ["VIC", "WA"];
+
+//********************************* SETTINGS END********************************** */
+
+// variables for storage and filtering
+let rawCSVData,
+  rejectedStates,
+  chartjsdata = [];
+let csvSplitByMonth = [];
+let showOverlay = false;
 
 //Reference months, [0] left empty to line up with calender month equivalent Jan == 1
 const calendarMonths = [
@@ -37,9 +51,9 @@ const calendarMonths = [
   "September",
 ];
 
-//Update URL as required
-let latestCSVUrl =
-  "https://raw.githubusercontent.com/M3IT/COVID-19_Data/master/Data/COVID_AU_state_daily_change.csv";
+document.getElementById("header-title").innerHTML = headerTitle;
+document.getElementById("footer").innerHTML = footer;
+document.getElementById("footer2").innerHTML = footer2;
 
 /* 
 Fetch the latest csv data from the Github page
@@ -54,16 +68,14 @@ Papa.parse(latestCSVUrl, {
   download: true,
   complete: function (results) {
     rawCSVData = results.data;
-    //console.log(rawCSVData);
     createCSVObject();
+    displaySubHeader();
     extractRecovered();
-
-    chartjsdata.push(csvSplitByMonth[0].vicRecoveredChartData);
+    chartjsdata.push(csvSplitByMonth[0].vicRecoveredChartData); //add the recovered totals so they dont need to be calculated again for display
     chartjsdata.push(csvSplitByMonth[0].WARecoveredChartData);
-    //console.log(csvSplitByMonth[0].vicRecoveredChartData);
-    //  console.log("chart", chartjsdata);
-    // createChart();
     displayCharts();
+
+    fillList();
     console.log(csvSplitByMonth);
   },
 });
@@ -130,19 +142,20 @@ function extractRecovered() {
       //begin looping through the months
       monthTotal = csvSplitByMonth[index][calendarMonths[index]][state].raw
         .map((day) => {
+          // Go through each day and store recovered
           let _recoveredTotal = parseInt(day.recovered);
           return _recoveredTotal;
         })
         .reduce((accum, recovered) => {
+          //Total the recovered
           return accum + recovered;
         });
 
       csvSplitByMonth[index][calendarMonths[index]][
         state
-      ].recovered = monthTotal;
+      ].recovered = monthTotal; //Write the months total to the object
       if (index <= calendarMonths.length + 1 && state == "VIC") {
-        //split each state into its own array
-        vicRecoveredChartData.push(monthTotal);
+        vicRecoveredChartData.push(monthTotal); //split each state into its own array
       } else {
         WARecoveredChartData.push(monthTotal);
       }
@@ -155,25 +168,35 @@ function extractRecovered() {
   csvSplitByMonth[0] = yearlyTotals; // save to the reference Object
 }
 
-/* Begin filtering by 
-month */
+/* Goes through the states array and charts the data for each state to the display */
 
-function displayCharts(state) {
+function displayCharts(state, overlay) {
   let colours = [];
+  let lineState = 1;
+  let lineDataset = {
+    label: labelText,
+    data: chartjsdata[lineState],
+    backgroundColor: colours,
+    fill: false,
+    borderColor: "white",
+    borderWidth: 2,
+    hoverBorderWidth: 4,
+    hoverBorderColor: "#FFF",
+    pointBorderColor: "red",
+    type: "line",
+  };
 
   states.map((state) => {
     let currentState = states.findIndex((states) => states === state); //avoid hard coding states
-    recoveryText += " <local time in Victoria>";
-    console.log(state);
-    var ctx = document.getElementById(state.toLowerCase()).getContext("2d");
-    console.log(ctx);
+    let ctx = document.getElementById(state.toLowerCase()).getContext("2d"); //match state array to html id
     let _months = calendarMonths.slice(1);
-    console.log(
-      "asdasdasd>>>",
-      states.findIndex((states) => states === state)
-    );
+
+    Chart.defaults.global.defaultFontFamily = "Lato";
+    Chart.defaults.global.defaultFontSize = 14;
+    Chart.defaults.global.defaultFontColor = "white";
 
     chartjsdata[currentState].map((dayRecVal) => {
+      //go through each months data and push a colour according to the colour in settings
       if (dayRecVal < medColour) {
         colours.push(chartjsBarColours[0]);
       } else if (dayRecVal >= medColour && dayRecVal <= highColour) {
@@ -182,46 +205,167 @@ function displayCharts(state) {
         colours.push(chartjsBarColours[2]);
       }
     });
-    Chart.defaults.global.defaultFontFamily = "Lato";
-    Chart.defaults.global.defaultFontSize = 14;
-    Chart.defaults.global.defaultFontColor = "Red";
-    let vicChart = new Chart(ctx, {
+
+    //create dataset
+    let _datatsets = [
+      {
+        label: labelText,
+        data: chartjsdata[currentState], //dynamically get data for each state
+        backgroundColor: colours,
+        borderColor: "white",
+        borderWidth: 1,
+        hoverBorderWidth: 4,
+        hoverBorderColor: "#FFF",
+      },
+    ];
+
+    if (state == "VIC" && overlay == true) {
+      //if overlay is selected, push onto the dataset array
+      _datatsets.push(lineDataset);
+    } else if (overlay == true) {
+      lineDataset = {
+        ...lineDataset,
+        data: chartjsdata[0],
+      };
+      _datatsets.push(lineDataset);
+    }
+
+    new Chart(ctx, {
       type: "bar",
       data: {
         labels: _months,
-        datasets: [
-          {
-            label: recoveryText,
-            data: chartjsdata[currentState],
-            backgroundColor: colours,
-            borderColor: colours,
-            borderWidth: 2,
-            hoverBorderWidth: 4,
-            hoverBorderColor: "#000",
-          },
-        ],
+        datasets: _datatsets,
       },
       options: {
         title: {
           display: true,
-          text: state + " Recovered",
+          text: getTime(state),
           fontSize: 18,
         },
         legend: {
           // position: "right",
+          display: false,
+
           labels: {
             fontColor: "#000",
           },
-          layout: {
-            padding: {
-              right: 500,
-              left: 0,
-              top: 0,
-              bottom: 0,
-            },
-          },
+          layout: {},
         },
       },
     });
   });
 }
+
+/*
+This function creates the sub header pulling the first and last date
+*/
+
+function displaySubHeader() {
+  let lastMonth = csvSplitByMonth.length - 2;
+
+  const earliestDataDate =
+    csvSplitByMonth[1][calendarMonths[1]][states[0]].raw[0].date;
+
+  console.log("sdsdsdsd", earliestDataDate);
+
+  let lastDay =
+    csvSplitByMonth[lastMonth][calendarMonths[lastMonth]][states[0]].raw
+      .length - 1;
+  const latestDataDate =
+    csvSplitByMonth[lastMonth][calendarMonths[lastMonth]][states[0]].raw[
+      lastDay
+    ].date;
+  setTimeout(function () {
+    document.getElementById("header-subtitle").innerHTML =
+      "Data : " + earliestDataDate + " to " + latestDataDate;
+    document.getElementById("header-subtitle").className = " text-light";
+  }, 750);
+}
+
+/*
+Fills the months for the select listbox
+*/
+
+function fillList() {
+  const listBox = document.querySelector("#months-list");
+  for (let index = 1; index <= calendarMonths.length; index++) {
+    let newOption = new Option(calendarMonths[index], calendarMonths[index]);
+    listBox.appendChild(newOption);
+  }
+}
+
+/*
+Function gets called to set the time for the chart title
+*/
+function getTime(state) {
+  state = state == "VIC" ? "Melbourne" : "Perth"; //set the timezone dynamically
+  let requestedTimezone = "Australia/" + state;
+
+  let requestedTime = new Date().toLocaleString("en-US", {
+    timeZone: requestedTimezone,
+  });
+
+  return state + " Recovered @ " + requestedTime; //return text + time
+}
+
+/*
+Handle displaying the raw data and selectboxes
+*/
+
+const overlayButton = document.querySelector("#overlay-selection");
+const selectElement = document.querySelector("#months-list");
+
+overlayButton.addEventListener("click", (event) => {
+  showOverlay = !showOverlay;
+
+  console.log("sdsd", event.srcElement);
+
+  document.getElementById("overlay-selection").innerHTML = showOverlay
+    ? "Remove Overlay"
+    : "Show State Overlay";
+  if (showOverlay) {
+    displayCharts("VIC", true);
+  } else {
+    displayCharts("VIC", false);
+  }
+});
+
+selectElement.addEventListener("change", (event) => {
+  let monthIndex = csvSplitByMonth.findIndex((index) => {
+    return Object.keys(index) == event.target.value;
+  });
+
+  const selectBoxV = document.querySelector("#raw-list-v");
+  const selectBoxW = document.querySelector("#raw-list-w");
+  $("#raw-list-v").empty();
+  $("#raw-list-w").empty();
+
+  document.getElementById("vic-recovered-text").innerHTML =
+    "Total recovered in Vic : " +
+    csvSplitByMonth[0].vicRecoveredChartData[monthIndex - 1];
+  document.getElementById("wa-recovered-text").innerHTML =
+    "Total recovered in WA : " +
+    csvSplitByMonth[0].WARecoveredChartData[monthIndex - 1];
+  for (state in states) {
+    for (
+      let index = 1;
+      index <=
+      csvSplitByMonth[monthIndex][calendarMonths[monthIndex]][states[state]].raw
+        .length;
+      index++
+    ) {
+      let newOption = new Option(
+        JSON.stringify(
+          csvSplitByMonth[monthIndex][calendarMonths[monthIndex]][states[state]]
+            .raw[index]
+        ),
+        index
+      );
+      if (state == 0) {
+        selectBoxV.appendChild(newOption);
+      } else {
+        selectBoxW.appendChild(newOption);
+      }
+    }
+  }
+});
